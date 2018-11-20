94dde3282fd7e77ca9a39da860eb2b7b1e7a4295
Dave Ankin <alexanderankin@gmail.com>
2018-11-20 11:32:27 -0500

    region and map

--- db/install.js
+++ db/install.js
@@ -12,6 +12,21 @@ function wipe(done) {
       t.string('email');
       t.string('phone');
     })
+
+    .dropTableIfExists('map')
+    .dropTableIfExists('region')
+    .createTable('map', function(t) {
+      t.increments('id');
+      t.string('geojson', 500);
+      t.string('feature_key');
+      t.string('notes', 1000);
+    })
+    .createTable('region', function(t) {
+      t.increments('id');
+      t.integer('map_id').references('id').inTable('map');
+      t.string('notes', 1000);
+    })
+
     .asCallback(function (err, result) {
       knexRe.destroy(done);
     });
